499e82de00dce018efbac64c36b34fff1fef3962
yhutter <hutteryannick@gmail.com>
2025-10-03 16:57:52 +0200

    Render first sample data, add HDR Environment Maps and further tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { ACESFilmicToneMapping } from "three"
+import { HDRLoader, OrbitControls } from "three/examples/jsm/Addons.js"
 import * as THREE from "three/webgpu"
 import { Pane } from "tweakpane"
 
@@ -13,8 +15,21 @@ let mesh = null
 let pane = null
 let tweaksFolder = null
 let tweaks = {
-    backgroundColor: new THREE.Color(0x000000)
+    wireframe: false,
+    background: new THREE.Color(0x000000),
+    animate: false,
+    toneMappingOptions: {
+        "None": THREE.NoToneMapping,
+        "Agx": THREE.AgXToneMapping,
+        "Filmic": THREE.ACESFilmicToneMapping,
+        "Reinhard": THREE.ReinhardToneMapping,
+    },
+    toneMapping: THREE.ACESFilmicToneMapping
 }
+let textureLoader = null
+let hdrLoader = null
+let controls = null
+let elapsedTime = 0
 
 window.onload = async () => {
     const canvas = document.getElementById("app")
@@ -25,30 +40,59 @@ window.onload = async () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setClearColor(tweaks.backgroundColor)
 
     const pixelRatio = Math.min(2, window.devicePixelRatio)
     renderer.setPixelRatio(pixelRatio)
+    renderer.setClearColor(tweaks.background)
+    renderer.toneMapping = tweaks.toneMapping
+
+    hdrLoader = new HDRLoader()
 
     await renderer.init()
 
     const aspect = sizes.width / sizes.height
     camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000)
-    camera.position.set(0, 0, 2)
+    camera.position.set(0, 1, 1)
+
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
 
     scene = new THREE.Scene()
 
-    const geo = new THREE.BoxGeometry(1, 1, 1)
-    const mat = new THREE.MeshBasicNodeMaterial({ color: 0xffffff })
+
+    textureLoader = new THREE.TextureLoader()
+    const texture = await textureLoader.loadAsync("/static/maps/texture.png")
+    texture.colorSpace = THREE.SRGBColorSpace
+
+    const heightMap = await textureLoader.loadAsync("/static/maps/height.png")
+
+    const size = 1
+    const resolution = 512
+    const geo = new THREE.PlaneGeometry(size, size, resolution, resolution)
+    const mat = new THREE.MeshStandardNodeMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+        wireframe: tweaks.wireframe,
+        map: texture,
+        displacementMap: heightMap,
+    })
 
     mesh = new THREE.Mesh(geo, mat)
+    mesh.geometry.rotateX(-Math.PI * 0.5)
+    camera.lookAt(mesh.position)
     scene.add(mesh)
+
+    const envMap = await hdrLoader.loadAsync("/static/maps/envmap-1k.hdr")
+    envMap.mapping = THREE.EquirectangularReflectionMapping
+    scene.environment = envMap
 
     clock = new THREE.Clock()
 
     pane = new Pane()
     tweaksFolder = pane.addFolder({ title: "Swiss Terrain 3D", expanded: true })
-    tweaksFolder.addBinding(tweaks, "backgroundColor", {
+
+    // Add Tweaks
+    tweaksFolder.addBinding(tweaks, "background", {
         label: "Background Color",
         view: "color",
         color: { type: "float" }
@@ -56,8 +100,24 @@ window.onload = async () => {
         renderer.setClearColor(e.value)
     })
 
-    window.addEventListener("resize", resize)
+    tweaksFolder.addBinding(tweaks, "toneMapping", {
+        label: "Tone Mapping",
+        options: tweaks.toneMappingOptions
+    }).on("change", e => {
+        renderer.toneMapping = e.value
+    })
 
+    tweaksFolder.addBinding(tweaks, "wireframe", {
+        label: "Wireframe",
+    }).on("change", e => {
+        mesh.material.wireframe = e.value
+    })
+
+    tweaksFolder.addBinding(tweaks, "animate", {
+        label: "Animate",
+    })
+
+    window.addEventListener("resize", resize)
 
     render()
 }
@@ -78,7 +138,14 @@ const resize = () => {
 const update = () => {
     const dt = clock.getDelta()
     const speed = 0.25
-    mesh.rotateY(Math.PI * dt * speed)
+    controls.update()
+
+    if (tweaks.animate) {
+        elapsedTime += dt
+        mesh.rotateY(Math.PI * dt * speed)
+
+    }
+
 }
 
 const render = () => {
