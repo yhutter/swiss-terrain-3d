82b5e36c4062f0569c88da965b6e09b91ee9e682
yhutter <hutteryannick@gmail.com>
2025-11-24 18:41:34 +0100

    Move logic from App into Terrain

--- src/code/App.ts
+++ src/code/App.ts
@@ -1,38 +1,23 @@
-import { HDRLoader, OrbitControls } from "three/examples/jsm/Addons.js"
+import { HDRLoader } from "three/examples/jsm/Addons.js"
 import * as THREE from "three"
 import { Pane, FolderApi } from "tweakpane"
 import { Terrain } from "./Terrain/Terrain"
-import { Player } from "./Player"
 import { InputHandler } from "./Utils/InputHandler"
-import { QuadTree } from "./QuadTree/QuadTree"
-import { QuadTreeHelper } from "./QuadTree/QuadTreeHelper"
 
 export class App {
-    // TODO: Make these paths selectable via dropdown in Tweakpane
-    private _terrainMetadataPath = "/static/data/output_tiles-sargans/terrain_metadata.json"
     private _envMapPath = "/static/maps/envmap-1k.hdr"
 
     // We receive the units in meters (for example 1000 meters). To have a more manageable scale in the 3D scene, we apply a render scale of 0.001 so that 1000 meters becomes 1 unit in the 3D scene.
     private _renderScale = 0.001
 
     private _renderer: THREE.WebGLRenderer
-    private _camera: THREE.PerspectiveCamera
-    private _defaultCameraPosition = new THREE.Vector3(0, 1, 2)
-    private _cameraQuadTreeVisualization: THREE.PerspectiveCamera
-    private _defaultQuadTreeVisualizationCameraPosition = new THREE.Vector3(0, 3, 0)
     private _scene: THREE.Scene
     private _clock: THREE.Clock
     private _pane: Pane
     private _tweaksFolder: FolderApi
     private _textureLoader: THREE.TextureLoader
     private _hdrLoader: HDRLoader
-    private _orbitControls: OrbitControls
     private _inputHandler: InputHandler
-
-    private _sizes = {
-        width: 0,
-        height: 0,
-    }
 
     private _tweaks = {
         background: new THREE.Color(0x000000),
@@ -43,14 +28,9 @@ export class App {
             "Reinhard": THREE.ReinhardToneMapping,
         },
         toneMapping: THREE.NoToneMapping,
-        enableQuadTreeVisualization: false,
     }
 
     private _terrain: Terrain | null = null
-    private _player: Player | null = null
-    private _playerStartPosition: THREE.Vector3 | null = null
-    private _quadTree: QuadTree | null = null
-    private _quadTreeHelper: QuadTreeHelper | null = null
 
 
     private static _instance: App
@@ -74,6 +54,14 @@ export class App {
         return this._scene
     }
 
+    get renderer(): THREE.WebGLRenderer {
+        return this._renderer
+    }
+
+    get aspect(): number {
+        return window.innerWidth / window.innerHeight
+    }
+
     get inputHandler(): InputHandler {
         return this._inputHandler
     }
@@ -89,9 +77,7 @@ export class App {
             canvas: canvas
         })
 
-        this._sizes.width = window.innerWidth
-        this._sizes.height = window.innerHeight
-        this._renderer.setSize(this._sizes.width, this._sizes.height)
+        this._renderer.setSize(window.innerWidth, window.innerHeight)
 
         const pixelRatio = Math.min(2, window.devicePixelRatio)
         this._renderer.setPixelRatio(pixelRatio)
@@ -99,17 +85,6 @@ export class App {
         this._renderer.toneMapping = this._tweaks.toneMapping
 
         this._hdrLoader = new HDRLoader()
-
-        const aspect = this._sizes.width / this._sizes.height
-
-        this._camera = new THREE.PerspectiveCamera(75, aspect, 0.01, 1000)
-        this._camera.position.copy(this._defaultCameraPosition)
-
-        this._cameraQuadTreeVisualization = new THREE.PerspectiveCamera(75, aspect, 0.01, 1000)
-        this._cameraQuadTreeVisualization.position.copy(this._defaultQuadTreeVisualizationCameraPosition)
-
-        this._orbitControls = new OrbitControls(this._camera, this._renderer.domElement)
-        this._orbitControls.enableDamping = true
 
         this._scene = new THREE.Scene()
 
@@ -124,7 +99,8 @@ export class App {
     }
 
     async run(): Promise<void> {
-        await this.setupTerrain()
+        this._terrain = new Terrain()
+        await this._terrain.initialize()
 
         // No point in continuing if terrain failed to load
         if (!this._terrain) {
@@ -132,63 +108,17 @@ export class App {
             return
         }
 
-        this.setupPlayer()
-
-        const maxDepth = this._terrain.maxLevel
-        this._quadTree = new QuadTree(this._terrain.boundingBox!, maxDepth)
-
-        this._quadTreeHelper = new QuadTreeHelper(this._quadTree)
-        this._scene.add(this._quadTreeHelper)
-
-
-        const terrainCenter = this._terrain!.center
-        this._camera.lookAt(terrainCenter)
-
         this.setupHDREnvironment()
         this.setupTweaks()
 
-        this.toggleQuadTreeVisualization(this._tweaks.enableQuadTreeVisualization)
-
         window.addEventListener("resize", () => this.onResize())
-
         this.render()
-    }
-
-    private setupPlayer(): void {
-        if (!this._terrain) return
-
-        const terrainCenter = this._terrain.center
-        this._playerStartPosition = new THREE.Vector3(
-            terrainCenter.x + 1,
-            0.0,
-            terrainCenter.z + 1,
-        )
-        this._player = new Player(this._playerStartPosition)
-        this._cameraQuadTreeVisualization.position.x = this._playerStartPosition.x
-        this._cameraQuadTreeVisualization.position.z = this._playerStartPosition.z
-        this._cameraQuadTreeVisualization.rotation.x = -Math.PI / 2
-    }
-
-    private async setupTerrain(): Promise<void> {
-        this._terrain = new Terrain()
-        await this._terrain.initialize(this._terrainMetadataPath)
     }
 
     private async setupHDREnvironment(): Promise<void> {
         const envMap = await this._hdrLoader.loadAsync(this._envMapPath)
         envMap.mapping = THREE.EquirectangularReflectionMapping
         this._scene.environment = envMap
-    }
-
-    private toggleQuadTreeVisualization(enabled: boolean): void {
-        if (enabled) {
-            this._quadTreeHelper!.visible = true
-            this._terrain!.shouldUseDemTexture = false
-        }
-        else {
-            this._quadTreeHelper!.visible = false
-            this._terrain!.shouldUseDemTexture = true
-        }
     }
 
     private setupTweaks(): void {
@@ -207,45 +137,24 @@ export class App {
             this._renderer.toneMapping = e.value
         })
 
-        this._tweaksFolder.addBinding(this._tweaks, "enableQuadTreeVisualization", {
-            label: "Enable QuadTree Visualization"
-        }).on("change", (e) => {
-            this.toggleQuadTreeVisualization(e.value)
-        })
     }
 
     private onResize(): void {
-        this._sizes.width = window.innerWidth
-        this._sizes.height = window.innerHeight
-
-        this._renderer.setSize(this._sizes.width, this._sizes.height)
+        this._renderer.setSize(window.innerWidth, window.innerHeight)
         const pixelRatio = Math.min(2, window.devicePixelRatio)
         this._renderer.setPixelRatio(pixelRatio)
-
-        const aspect = this._sizes.width / this._sizes.height
-        this._camera.aspect = aspect
-        this._camera.updateProjectionMatrix()
-
-        this._cameraQuadTreeVisualization.aspect = aspect
-        this._cameraQuadTreeVisualization.updateProjectionMatrix()
+        this._terrain?.onResize(this.aspect)
     }
 
     private update() {
         const dt = this._clock.getDelta()
-        this._orbitControls.update()
-        this._player?.update(dt)
-        this._quadTree?.insertPosition(this._player!.position2D)
-        this._quadTreeHelper?.update()
-        this._terrain?.update(dt, this._quadTree!.getChildren())
+        this._terrain?.update(dt)
 
-        // Track player position
-        this._cameraQuadTreeVisualization.position.x = this._player!.position.x
-        this._cameraQuadTreeVisualization.position.z = this._player!.position.z
     }
 
     private render() {
         this.update()
-        const camera = this._tweaks.enableQuadTreeVisualization ? this._cameraQuadTreeVisualization : this._camera
+        const camera = this._terrain!.activeCamera
         this._renderer.render(this._scene, camera)
         window.requestAnimationFrame(() => this.render())
     }
--- src/code/terrain/Terrain.ts
+++ src/code/terrain/Terrain.ts
@@ -3,17 +3,33 @@ import { App } from '../App';
 import { TerrainTile } from "./TerrainTile";
 import { TerrainMetadata } from "./TerrainMetadata";
 import { TerrainTileManager } from "./TerrainTileManager";
-import { QuadTreeNode } from "../QuadTree/QuadTreeNode";
+import { QuadTree } from "../QuadTree/QuadTree";
+import { QuadTreeHelper } from "../QuadTree/QuadTreeHelper";
+import { Player } from '../Player';
+import { OrbitControls } from "three/examples/jsm/Addons.js"
 
 export class Terrain extends THREE.Group {
     private _tweaks = {
         wireframe: false,
         anisotropy: 16,
+        enableQuadTreeVisualization: true,
     }
+
+    // TODO: Make these paths selectable via dropdown in Tweakpane
+    private _terrainMetadataPath = "/static/data/output_tiles-sargans/terrain_metadata.json"
 
     private _terrainTiles: TerrainTile[] = []
     private _metadata: TerrainMetadata | null = null
     private _shouldUseDemTexture: boolean = false
+    private _camera: THREE.PerspectiveCamera
+    private _defaultCameraPosition = new THREE.Vector3(0, 1, 2)
+    private _cameraQuadTreeVisualization: THREE.PerspectiveCamera
+    private _defaultQuadTreeVisualizationCameraPosition = new THREE.Vector3(0, 3, 0)
+    private _player: Player | null = null
+    private _playerStartPosition: THREE.Vector3 | null = null
+    private _quadTree: QuadTree | null = null
+    private _quadTreeHelper: QuadTreeHelper | null = null
+    private _orbitControls: OrbitControls
 
     get center(): THREE.Vector3 {
         if (!this._metadata) {
@@ -42,8 +58,25 @@ export class Terrain extends THREE.Group {
         return this._metadata.bboxWorldSpace
     }
 
+    get activeCamera(): THREE.PerspectiveCamera {
+        if (this._tweaks.enableQuadTreeVisualization) {
+            return this._cameraQuadTreeVisualization
+        }
+        return this._camera
+    }
+
     constructor() {
         super()
+
+        const aspect = App.instance.aspect
+        this._camera = new THREE.PerspectiveCamera(75, aspect, 0.01, 1000)
+        this._camera.position.copy(this._defaultCameraPosition)
+
+        this._cameraQuadTreeVisualization = new THREE.PerspectiveCamera(75, aspect, 0.01, 1000)
+        this._cameraQuadTreeVisualization.position.copy(this._defaultQuadTreeVisualizationCameraPosition)
+
+        this._orbitControls = new OrbitControls(this._camera, App.instance.renderer.domElement)
+        this._orbitControls.enableDamping = true
         this.setupTweaks()
     }
 
@@ -54,18 +87,61 @@ export class Terrain extends THREE.Group {
         }
     }
 
-    async initialize(metadataPath: string): Promise<void> {
-        await TerrainTileManager.initializeFromMetadata(metadataPath)
-        this._metadata = TerrainTileManager.terrainMetadata!
+    async initialize(): Promise<void> {
+        await TerrainTileManager.initializeFromMetadata(this._terrainMetadataPath)
+        this._metadata = TerrainTileManager.terrainMetadata
+        if (!this._metadata) {
+            console.error("Terrain: Failed to load terrain metadata!")
+            return
+        }
+
+        const maxDepth = this.maxLevel
+        this._quadTree = new QuadTree(this.boundingBox!, maxDepth)
+
+        this._quadTreeHelper = new QuadTreeHelper(this._quadTree)
+        this.add(this._quadTreeHelper)
+
+        this._camera.lookAt(this.center)
+
+        this._playerStartPosition = new THREE.Vector3(
+            this.center.x,
+            0.0,
+            this.center.z,
+        )
+
+        this._player = new Player(this._playerStartPosition)
+        this._cameraQuadTreeVisualization.position.x = this._playerStartPosition.x
+        this._cameraQuadTreeVisualization.position.z = this._playerStartPosition.z
+        this._cameraQuadTreeVisualization.rotation.x = -Math.PI / 2
+
+        this.toggleQuadTreeVisualization(this._tweaks.enableQuadTreeVisualization)
+
         App.instance.scene.add(this)
     }
 
-    onResize(sizes: { width: number; height: number }): void {
-        // No implementation needed for terrain on resize
+    onResize(aspect: number): void {
+        this._camera.aspect = aspect
+        this._camera.updateProjectionMatrix()
+
+        this._cameraQuadTreeVisualization.aspect = aspect
+        this._cameraQuadTreeVisualization.updateProjectionMatrix()
     }
 
-    update(dt: number, quadTreeNodes: QuadTreeNode[]) {
+    update(dt: number) {
+        this._orbitControls.update()
+        this._player?.update(dt)
+        this._quadTree?.insertPosition(this._player!.position2D)
+        this._quadTreeHelper?.update()
+
+        const quadTreeNodes = this._quadTree?.getChildren() || []
         const resolution = 33
+
+        // Track player position
+        if (this._tweaks.enableQuadTreeVisualization) {
+            this._cameraQuadTreeVisualization.position.x = this._player!.position.x
+            this._cameraQuadTreeVisualization.position.z = this._player!.position.z
+        }
+
         // Figure out which tiles we can remove, e.g. tiles that are not in the quadTreeNodes.
         for (const tile of this._terrainTiles) {
             const foundNode = quadTreeNodes.find(node => node.id === tile.id)
@@ -93,6 +169,17 @@ export class Terrain extends THREE.Group {
                 // TODO: Fix this warning here
                 this.add(tile.mesh)
             })
+        }
+    }
+
+    private toggleQuadTreeVisualization(enabled: boolean): void {
+        if (enabled) {
+            this._quadTreeHelper!.visible = true
+            this.shouldUseDemTexture = false
+        }
+        else {
+            this._quadTreeHelper!.visible = false
+            this.shouldUseDemTexture = true
         }
     }
 
@@ -126,6 +213,12 @@ export class Terrain extends THREE.Group {
                     tile.material.needsUpdate = true
                 }
             }
+        })
+
+        folder.addBinding(this._tweaks, "enableQuadTreeVisualization", {
+            label: "Enable QuadTree Visualization"
+        }).on("change", (e) => {
+            this.toggleQuadTreeVisualization(e.value)
         })
     }
 }
