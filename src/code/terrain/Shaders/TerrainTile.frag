de41045914ce3af818358e0f4e0fc23579218214
yhutter <hutteryannick@gmail.com>
2026-08-13 11:38:40 +0200

    Setup Jai build system

--- src/code/terrain/Shaders/TerrainTile.frag
+++
@@ -1,6 +0,0 @@
-varying vec2 vUv;
-uniform sampler2D uDopTexture;
-void main() {
-    vec4 baseColor = texture2D(uDopTexture, vUv);
-    csm_DiffuseColor = baseColor;
-}
--- src/code/terrain/TerrainMetadata.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three"
-import { TerrainLevelMetadata } from "./TerrainLevelMetadata"
-
-export type TerrainMetadata = {
-    bboxWorldSpace: THREE.Box2,
-    globalMinElevation: number,
-    globalMaxElevation: number,
-    levels: TerrainLevelMetadata[]
-}
