da65fa146ec4091d9fbf969e3c80d79be500669e
yhutter <hutteryannick@gmail.com>
2026-08-30 11:25:56 +0200

    Revert "Switch to Sokol"
    This reverts commit 53e934419b7891319961876768fa3702510ce279.

--- src/main.c
+++ src/main.c
@@ -1,78 +1,134 @@
-#include <stdio.h>
-#include "sokol_app.h"
-#include "sokol_gfx.h"
-#include "sokol_log.h"
-#include "sokol_glue.h"
-#include "cimgui.h"
-#include "sokol_imgui.h"
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <stdlib.h>
+
+#define WIDTH 1280
+#define HEIGHT 960
+#define CLEAR_COLOR SDL_COLOR(0x091B1EFF)
+
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_GPUDevice* device;
+    TTF_Font* font;
+    const char* base_path;
+} AppContext;
 
 
-static struct {
-    sg_pass_action pass_action;
-} state;
+static AppContext* app_context = NULL;
 
-static void init(void) {
-    sg_setup(&(sg_desc) {
-        .environment = sglue_environment(),
-        .logger.func = slog_func
-    });
+#define SDL_COLOR(hex) \
+    ((SDL_FColor) { \
+        ((((uint32_t)hex >> 24) & 0xFF) / 255.0f),\
+        ((((uint32_t)hex >> 16) & 0xFF) / 255.0f),\
+        ((((uint32_t)hex >> 8) & 0xFF) / 255.0f),\
+        ((((uint32_t)hex >> 0) & 0xFF) / 255.0f)\
+    })
 
-    simgui_setup(&(simgui_desc_t) {0});
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+    app_context = malloc(sizeof(AppContext));
+    app_context->window = NULL;
+    app_context->renderer = NULL;
+    app_context->device = NULL;
+    app_context->font = NULL;
 
-    state.pass_action = (sg_pass_action) {
-        .colors[0] = { 
-            .load_action = SG_LOADACTION_CLEAR,
-            .clear_value = {0.0f, 0.5f, 1.0f, 1.0f}
+    if(!SDL_SetAppMetadata("Swiss Terrain 3D", "0.0.1", "ch.yhutter.swissterrain3d")) {
+        SDL_Log("Failed to set app metadata: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        SDL_Log("Could not initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_WindowFlags window_flags = SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE;
+    if (!SDL_CreateWindowAndRenderer("Swiss Terrain 3D", WIDTH, HEIGHT, window_flags, &app_context->window, &app_context->renderer)) {
+        SDL_Log("Could not create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_GPUShaderFormat shader_format = SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_MSL;
+    app_context->device = SDL_CreateGPUDevice(shader_format, false, NULL);
+    if (!app_context->device) {
+        SDL_Log("Failed to create gpu device: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_ClaimWindowForGPUDevice(app_context->device, app_context->window)) {
+        SDL_Log("Failed to claim window for gpu device: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_SetRenderLogicalPresentation(app_context->renderer, WIDTH, HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    if (!TTF_Init()) {
+        SDL_Log("Failed to initialize SDL_ttf: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    app_context->base_path = SDL_GetBasePath();
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        SDL_Keycode key = event->key.key;
+        if (key == SDLK_ESCAPE) {
+            return SDL_APP_SUCCESS;
         }
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate) {
+    SDL_GPUCommandBuffer* command_buffer = SDL_AcquireGPUCommandBuffer(app_context->device);
+    if (!command_buffer) {
+        SDL_Log("Failed to acquire command buffer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_GPUTexture* swapchain_texture = NULL;
+    if (!SDL_WaitAndAcquireGPUSwapchainTexture(command_buffer, app_context->window, &swapchain_texture, NULL, NULL)) {
+        SDL_Log("Failed to acquire swap chain texture: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_GPUColorTargetInfo target_info = {
+        .texture = swapchain_texture,
+        .cycle = true,
+        
+        // Clear the texture to a known color
+        .load_op = SDL_GPU_LOADOP_CLEAR,
+
+        // Keep the rendered output
+        .store_op = SDL_GPU_STOREOP_STORE,
+
+        .clear_color = CLEAR_COLOR
     };
+
+    SDL_GPURenderPass* render_pass = SDL_BeginGPURenderPass(command_buffer, &target_info, 1, NULL);
+    SDL_EndGPURenderPass(render_pass);
+
+    SDL_SubmitGPUCommandBuffer(command_buffer);
+
+    return SDL_APP_CONTINUE;
 }
 
-static void frame(void) {
-    simgui_new_frame(&(simgui_frame_desc_t) {
-        .width = sapp_width(),
-        .height = sapp_height(),
-        .delta_time = sapp_frame_duration(),
-        .dpi_scale = sapp_dpi_scale()
-    });
-
-    igSetNextWindowPos((ImVec2) {10, 10}, ImGuiCond_Once);
-    igSetNextWindowSize((ImVec2) {400, 100}, ImGuiCond_Once);
-
-    igBegin("Swiss Terrain 3D", 0, ImGuiWindowFlags_None);
-    igColorEdit3("Background", &state.pass_action.colors[0].clear_value.r, ImGuiColorEditFlags_None);
-    igEnd();
-
-    sg_begin_pass(&(sg_pass) {
-        .action = state.pass_action,
-        .swapchain = sglue_swapchain()
-    });
-    simgui_render();
-    sg_end_pass();
-
-    sg_commit();
-}
-
-static void cleanup(void) {
-    simgui_shutdown();
-    sg_shutdown();
-}
-
-static void event(const sapp_event* ev) {
-    simgui_handle_event(ev);
-}
-
-sapp_desc sokol_main(int argc, char* argv[]) {
-    return (sapp_desc) {
-        .init_cb = init,
-        .frame_cb = frame,
-        .cleanup_cb = cleanup,
-        .event_cb = event,
-        .window_title = "Swiss Terrain 3D",
-        .width = 1920,
-        .height = 1080,
-        .depth_format = SAPP_PIXELFORMAT_NONE,
-        .icon.sokol_default = true,
-        .logger.func = slog_func
-    };
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    TTF_CloseFont(app_context->font);
+    SDL_ReleaseWindowFromGPUDevice(app_context->device, app_context->window);
+    SDL_DestroyGPUDevice(app_context->device);
+    SDL_DestroyRenderer(app_context->renderer);
+    SDL_DestroyWindow(app_context->window);
+    TTF_Quit();
+    SDL_Quit();
+    free(app_context);
 }
 
